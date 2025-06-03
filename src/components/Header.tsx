30ad9c0d614ac6d413d76263257974c4e07ad182
ecsunipe <secretaria.ecsunipe@gmail.com>
2025-06-03 20:36:56 -0300

    fix: Troca link do header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { ThemeButton } from '@/components/ThemeButton';
 export function Header() {
   return (
     <header className="sm:px-0 px-4 py-4 flex items-center justify-between">
-        <a href="https://ecs-notas.netlify.app/">
+        <a href="https://ecs-notas-6skz.vercel.app/">
           <img src="/ECSLogo.png" alt="Logo da ECS" width="150"/>
         </a>
       <div className="flex items-center justify-center gap-4">
